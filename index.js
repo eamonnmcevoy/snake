525100e8718a772fd2e4b5508a3729bed29c8f8f
Eamonn McEvoy <emcevoy@deloitte.nl>
2018-09-09 17:00:33 +0200

    create grid dataset

--- index.js
+++ index.js
@@ -6,17 +6,49 @@ class Grid {
     this.blockarea = blockarea;
     this.margin = margin;
     this.blocksize = blockarea - margin;
+
+    this.generateDataSet();
+  }
+
+  generateDataSet() {
+    this.dataset = [];
+    for (let x = 0; x < width; x++) {
+      this.dataset[x] = [height];
+      for (let y = 0; y < height; y++) {
+        this.setPoint({ x, y }, 'off');
+      }
+    }
+  }
+
+  setPoint(point, state) {
+    this.dataset[point.x][point.y] = state;
+  }
+
+  getPoint(point) {
+    return this.dataset[point.x][point.y];
   }
 
   render() {
     for(let x=0; x<width; x++) {
       for(let y=0; y<height; y++) {
-        const point = { x, y };
-        this.ctx.fillStyle = 'lightgrey';
+        const point = {x,y};
+        const state = this.getPoint(point);
+        this.ctx.fillStyle = this.getFillStyle(state);
         ctx.clearRect(point.x * blockarea, point.y * blockarea, blocksize, blocksize);
         ctx.fillRect(point.x * blockarea, point.y * blockarea, blocksize, blocksize);
       }
     }
+  }
+
+  getFillStyle(state) {
+    switch(state) {
+      case 'on':
+        return 'green';
+      case 'off':
+        return 'lightgrey';
+      default:
+        return 'lightgrey';
+    };
   }
 }
 
